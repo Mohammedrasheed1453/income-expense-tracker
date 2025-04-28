0a06f9a164f3d9ce04a57f5e3b350ce3ef4fc9d4
Mohammedrasheed1453 <shaikmohammedrasheed1453@gmail.com>
2025-04-28 21:41:59 +0530

    Added components folder and its files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react'; // Required for JSX
+import ReactDOM from 'react-dom'; // Correct for React 16
+import Details from './componenets/Details/Details';
+import Main from './componenets/Main/Main';
+import { Grid } from '@material-ui/core';
+import useStyles from './styles';
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Grid className={classes.grid}  container spacing={ 5} alignItems='center' style={{ height: '100vh' }}>
+        <Grid item xs={12} sm={4} className={classes.mobile}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3} className={classes.main}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.last}>
+        <Details title="Expense" />
+        </Grid>
+      </Grid>
+   
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; // Required for JSX
+import ReactDOM from 'react-dom'; // Correct for React 16
+
 import './index.css';
 import App from './App';
+import { Provider } from './context/context'; // Importing the Provider from context
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    
+<Provider> 
+    <App /> 
+</Provider>, 
+document.getElementById('root'));
